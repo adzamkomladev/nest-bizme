1c8f89d8a0b7d8a7d9299a116838c0f7d9c0bba6
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-07 23:27:27 +0000

    add config files and setup application to use .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.dev.env',
+    }),
+  ],
 })
 export class AppModule {}
