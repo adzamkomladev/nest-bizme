a45b16f52ef7a1272309afee248a3fbaf0df1bc0
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-08 03:51:36 +0000

    reset configurations using code and json, uninstall @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.dev.env',
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
 })
 export class AppModule {}
